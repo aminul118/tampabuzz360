250af61af4930f5f8f77b277bbb5e159eb473534
Md Aminul Islam <mr.aminul118@gmail.com>
2025-04-13 04:42:31 +0600

    Tampa news not found text visilization improve

--- src/components/TampaToday/NoNewsFound.tsx
+++ src/components/TampaToday/NoNewsFound.tsx
@@ -1,11 +1,11 @@
 import { Newspaper } from "lucide-react";
-import React from "react";
 
 const NoNewsFound = ({ category }: { category: string }) => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-284px)] gap-6">
-      <Newspaper className="text-red-500" size={80} />
-      <p className="text-3xl text-red-500 font-semibold">
+    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-284px)] gap-6 text-center">
+      <Newspaper className="text-red-500" size={70} />
+
+      <p className="text-2xl lg:text-3xl text-red-500 font-semibold">
         No News Found in {category} category
       </p>
     </div>
