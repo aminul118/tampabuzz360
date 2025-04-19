82b8547e1c99f5873b26101200f09750ad3b6ff5
Md Aminul Islam <mr.aminul118@gmail.com>
2025-04-20 01:19:15 +0600

    tag manager possitin top of the body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,10 +39,8 @@ export const metadata: Metadata = {
 const RootLayout = ({ children }: TChildren) => {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={poppins.className}>
-        <GoogleTagManager gtmId="GTM-WH7CWN2H" />
-        {children}
-      </body>
+      <GoogleTagManager gtmId="GTM-WH7CWN2H" />
+      <body className={poppins.className}>{children}</body>
     </html>
   );
 };
